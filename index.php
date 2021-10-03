8ded07be57fb38b9a8f7ae858c31c4be5ede4e88
Marc Morente <mmorente@inthinking.co.uk>
2021-10-04 01:15:49 +0200

    Output in Content-Type: text/text

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type: text/text');
 ini_set('MAX_EXECUTION_TIME', '-1');
 ini_set('memory_limit', '1G'); // or you could use 1G
 ini_set('display_errors', 1);
@@ -7,14 +8,12 @@ error_reporting(E_ALL);
 set_time_limit(0);
 require 'vendor/autoload.php';
 
-$filePath = 'tests/PGNFiles/test.pgn';
+$filePath = __DIR__.'/tests/PGNFiles/randomEvents.pgn';
 $pgn = new PGNParser\PGN($filePath);
 $games = $pgn->getGames();
 
-foreach ($games as $game) {
-    echo $pgn->metaData($game)->getBlackElo().'<br><br>';
-}
+/*foreach ($games as $game) {
+    echo $pgn->metaData($game)->getBlackElo()."\n";
+}*/
 
-/*echo '<pre>';
 print_r($games);
-echo '</pre>';*/
